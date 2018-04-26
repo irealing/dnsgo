4e0d72842315ec87f529d824a416eb14231715a7
Memory_Leak <irealing@163.com>
2018-04-26 12:52:31 +0800

    feat: 服务器，接收客户端DNS请求（不处理查询内容）

--- server/config.go
+++ server/config.go
@@ -1,16 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"log"
+)
+
 var (
 	cfg *Config = nil
 )
 
 type Config struct {
-	Host string
-	Port int
+	Host string `param:"host"`
+	Port int    `param:"port"`
+	Addr string `param:"-"`
 }
 
+func (c *Config) Validate() error {
+	c.Addr = fmt.Sprintf("%s:%d", c.Host, c.Port)
+	return nil
+}
 func init() {
+	log.Println("init config")
 	cfg = new(Config)
 	cfg.Host = ""
 	cfg.Port = 65533
+	cfg.Validate()
 }
